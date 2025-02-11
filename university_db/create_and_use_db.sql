a2856e75c2d4e8a0469c5be330d67b45b1fd085e
Leo <punzalank03@gmail.com>
2025-02-10 17:33:57 -0800

    Submitting ADVDBMS Week 02 - Session 01 - Exercise 01

--- university_db/create_and_use_db.sql
+++ university_db/create_and_use_db.sql
@@ -1,3 +1,10 @@
+
+-- Step 1: Create a database called UniversityDB
 CREATE DATABASE `UniversityDB`;
+
+-- Step 2: Use the newly created database
 USE `UniversityDB`;
+
+-- Verification (Optional)
+-- To confirm the active database
 SELECT DATABASE();
--- university_db/drop_db.sql
+++ university_db/drop_db.sql
@@ -1,3 +1,6 @@
-USE `UniversityDB`;
-DROP DATABASE IF EXISTS `UniversityDB`;
 
+-- Step 3: (Optional) Drop the database if needed
+USE `UniversityDB`;
+
+-- The IF EXISTS clause prevents an error if the database doesn't exist
+DROP DATABASE IF EXISTS `UniversityDB`;
